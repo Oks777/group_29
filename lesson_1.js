f95065ad70582f0bc260d024c96bc30d69302a48
Oks777 <103444846+Oks777@users.noreply.github.com>
2022-05-29 23:15:49 +0300

    Add files via upload
    add lesson_1.js

---
+++ lesson_1.js
@@ -0,0 +1,43 @@
+let yolochka4 = 4;
+let yolochka6 = 6;
+let yolochka_name = " Zelyonaya"
+
+let result = yolochka4 + yolochka6;
+let result_2 = yolochka6 + yolochka_name
+
+// console.log(`result + ${yolochka4 + yolochka6}`)
+
+// console.log(result);
+// console.log(result_2);
+
+// let t = true
+// let f = false
+
+// console.log(t + f)
+
+// 
+
+let age = 2;
+let age2 = 12;
+
+let tt = typeof (age)
+let nn = typeof(age2)
+console.log(tt)
+
+if (tt == nn) {
+    if (age >= 18 && age < 60) {
+
+       console.log("Full content")
+    } 
+
+    else if (age < 18 ) {
+        console.log("Multiki")
+    } 
+
+    else if (age >= 60) {
+        console.log("Kultura")
+    }
+
+} else { 
+    console.log("Good luck!")
+}   
