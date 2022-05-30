09585b1572e7b1054cde67bf6c49dd1959970340
Oks777 <103444846+Oks777@users.noreply.github.com>
2022-05-30 23:13:24 +0300

    Add files via upload
    add lesson_2_for.js

---
+++ lesson_2_for.js
@@ -0,0 +1,40 @@
+
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i , "= FOR")
+// }
+
+// for (let temperature = 0; temperature < 10; temperature++){
+    
+//     if (temperature < 3) {
+//         console.log("temperature = ", temperature, "Holodno")
+//     }   else if (temperature >= 3 && temperature < 7) {
+//         console.log("temperature = ",  temperature, " = HOROSHO!!!")
+//     }  else {
+//         console.log("temperature = ",  temperature, " = Kapec!")
+    
+//         // console.log(temperature , " = FOR!")
+//     } 
+// }
+
+let names = ['Oksana',
+             'Ksenia',
+             'Pipiska',
+             'Yliay',
+             'Ignat']
+
+// for (item_1 of names) { 
+
+//   console.log(item_1)
+
+// }   
+
+for (item_1 of names){ 
+
+    if (item_1 == 'Yliay'){
+        break;
+    }
+
+  console.log(item_1)
+} 
+//break останавливает цикл на указанном значении
